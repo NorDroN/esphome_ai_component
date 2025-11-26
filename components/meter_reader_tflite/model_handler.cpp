e3ea46f7d167196e3badfe2ebd1ace149f826e9a
Andrey Klimov <nordron@live.ru>
2025-11-26 14:43:58 +0300

    Small fix

--- components/meter_reader_tflite/model_handler.cpp
+++ components/meter_reader_tflite/model_handler.cpp
@@ -1067,6 +1067,8 @@ bool ModelHandler::invoke_model(const uint8_t* input_data, size_t input_size) {
       output_size_ = output->dims->data[1] * output->dims->data[2];
       int num_params = output->dims->data[2];
 
+      float* output_data = output->data.f;
+
       ESP_LOGD(TAG, "Total size: %d, Num parameters: %d", output_size_, num_params);
 
       /* ---------------------------------------------------------------
